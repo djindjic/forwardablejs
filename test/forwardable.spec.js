3697403a372282723574bff5951e931934af3108
Aleksandar Djindjic <djindjic@gmail.com>
2015-01-05 22:45:25 +0100

    experiment with send

--- test/forwardable.spec.js
+++ test/forwardable.spec.js
@@ -2,13 +2,13 @@ import {
   Forwardable
 } from 'lib/index';
 
-describe('Forwardable', function(){
-  it('has method delegate', function(){
+describe('Forwardable', function() {
+  it('has method delegate', function() {
     let fwd = new Forwardable();
     expect(fwd).to.respondTo('delegate');
   });
 
-  it('assigns class with delegate method', function(){
+  it('assigns class with delegate method', function() {
     class Temp {
       constructor() {
         
@@ -20,7 +20,7 @@ describe('Forwardable', function(){
     expect(temp).to.respondTo('delegate');
   });
 
-  it('delegate method to receiver', function(){
+  it('delegate method to receiver', function() {
     class Temp {
       constructor() {
         this._records = [];
@@ -37,5 +37,69 @@ describe('Forwardable', function(){
     temp.push('first element');
 
     expect(temp.records).to.deep.equal(['first element']);
+  });
+
+  it('delegate get property to receiver', function() {
+    'getOwnPropertyDescriptors' in Object || (
+      Object.getOwnPropertyDescriptors = function (Object) {
+        var
+          gOPD    = Object.getOwnPropertyDescriptor,
+          gOPN    = Object.getOwnPropertyNames,
+          gOPS    = Object.getOwnPropertySymbols,
+          gNS     = gOPS ? function (object) {
+                             return gOPN(object).concat(gOPS(object));
+                           } :
+                           gOPN,
+          descriptors
+        ;
+        function copyFrom(key) {
+          descriptors[key] = gOPD(this, key);
+        }
+        return function getOwnPropertyDescriptors(object) {
+          var result = descriptors = {};
+          gNS(object).forEach(copyFrom, object);
+          descriptors = null;
+          return result;
+        };
+      }(Object)
+    );
+
+    class Receiver {
+      // constructor() { this._hello = ''; }
+      set hello(value) { this._hello = value; }
+      get hello() { return this._hello; }
+    }
+    class Temp {
+      constructor(receiver) {
+        this._receiver = receiver;
+        this.delegate('_receiver', 'hello');
+      }
+    }
+    Object.assign(Temp.prototype, Forwardable.prototype);
+
+    let receiver = new Receiver();
+    let d = Object.getOwnPropertyDescriptors(receiver);
+    for(elem in d) {
+      console.log('----');
+      console.log(elem);
+    }
+
+    receiver.hello = 'bbb';
+    var shallowCopy = Object.create(
+      Object.getPrototypeOf(receiver),
+      Object.getOwnPropertyDescriptors(receiver)
+    );
+
+    receiver.hello = 'aaa';
+    console.log(receiver.hello);
+    console.log(shallowCopy.hello);
+    // for(var p in receiver)
+    // {
+    //   console.log(p + ": " + receiver[p]); //if you have installed Firebug.
+    // }
+    // console.log(receiver);
+    let temp = new Temp(receiver);
+
+    expect(temp).to.equal('hello');
   });
 });
