cf5305f7a0994423bef8a58de8326cdb42d2e981
Aleksandar Djindjic <djindjic@gmail.com>
2015-01-10 15:32:35 +0100

    sauce

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -1,45 +1,19 @@
-//
-
-var geSaLaKaCuLa = require('gesalakacula');
-
-module.exports = function (config) {
-
-  var customLaunchers = geSaLaKaCuLa({
-    'Linux': {
-      'chrome': '37'
-    }
-  });
-
-  config.set({
-    basePath: '',
-    frameworks: ['jspm', 'mocha', 'chai'],
-    files: [
-      'test/*.spec.js'
-    ],
-    reporters: ['dots', 'saucelabs'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    singleRun: true,
-
-    browserDisconnectTimeout: 10 * 1000, // 10s
-    browserDisconnectTolerance: 2,
-    browserNoActivityTimeout: 2 * 60 * 1000, // 2m
-    captureTimeout: 0,
-
-    browsers: Object.keys(customLaunchers),
-    sauceLabs: {
-      testName: 'forwardablejs',
-      recordScreenshots: false
-    },
-    customLaunchers: customLaunchers
-  });
-
-
-  if (process.env.TRAVIS) {
-    var buildLabel = 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')';
-
-    config.sauceLabs.build = buildLabel;
-    config.sauceLabs.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
+var browsers = {                                 // 1
+  sl_ie_11: {
+    base: 'SauceLabs',
+    browserName: 'internet explorer',
+    platform: 'Windows 8.1',
+    version: '11'
   }
+};
+ 
+module.exports = function(config) {
+  config.set({
+    // all other options that are defined in
+    // local.karma.conf.js were elided for the
+    // purpose of this blog post.
+    reporters: ['saucelabs', 'spec'],            // 2
+    browsers: Object.keys(browsers),             // 3
+    customLaunchers: browsers                    // 4
+  });
 };
