ef0be039bd2dc57748b46aed5aa9e1bfb42ba852
Aleksandar Djindjic <djindjic@gmail.com>
2015-01-12 19:48:47 +0100

    0.1.28

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ gulp.task('deploy-master', function(){
     .on('end', shell.task([
             'git add --all',
             'git commit -m "' + newVer + '"', 
-            'git tag -a "' + newVer + '" -m "' + newVer + '"',
+            'git tag -' + newVer,
             'git push origin master', 
             'git push origin --tags'
            ]));
